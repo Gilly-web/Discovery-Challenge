d4f8b226b67a1cb6b164f0b84837d2098d6980a7
Gilly-web <gillytema@gmail.com>
2022-10-28 15:34:29 +0200

    Updated reading data through API

--- api.py
+++ api.py
@@ -1,3 +1,24 @@
-import requests
-response = requests.get('https://s3-eu-west-1.amazonaws.com/discovery-insure-interview-rev1/sightings_alerts.zip')
-print(response)
+#import requests
+#response = requests.get('https://s3-eu-west-1.amazonaws.com/discovery-insure-interview-rev1/sightings_alerts.zip')
+#print(response)
+from urllib import request
+import wget, zipfile, requests, csv, pandas as pd, seaborn as sns, numpy as np, matplotlib.pyplot as plt
+from io import BytesIO
+
+url = 'https://s3-eu-west-1.amazonaws.com/discovery-insure-interview-rev1/sightings_alerts.zip'
+
+req = requests.get(url)
+print('Download Complete')
+
+myZipfile = zipfile.ZipFile(BytesIO(req.content))
+myZipfile.extractall('Extracted')
+
+myCsvFile = pd.read_csv('Extracted/sightings_alerts/4cb82c2383ad.csv')
+#print(myCsvFile.head(10))
+
+
+#myHeatMap = sns.load_dataset(myCsvFile)
+myHeatMap = pd.pivot_table(data=myCsvFile, index='lat',columns='lon',values='acc')
+print(myHeatMap)
+sns.heatmap(myHeatMap, cmap="Blues")
+plt.show()
